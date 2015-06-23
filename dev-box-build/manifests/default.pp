31b1a686cd03ba59ce9616f03a2364cea800c41a
Alan Holt <aholt@vipasolutions.com>
2015-06-23 16:06:53 -0500

    progress

--- dev-box-build/manifests/default.pp
+++ dev-box-build/manifests/default.pp
@@ -4,29 +4,34 @@ node default {
   
   $davfs_conf_path = "/etc/davfs2/davfs2.conf"
   $path = "/usr/local/bin/:/bin/:/usr/bin/:/usr/local/sbin/:/usr/sbin/:/sbin/"
+  $cmd_timeout = 0
   $postgres_loc = "/etc/postgresql/9.3/main/"
   
   apt::ppa { 'ppa:kubuntu-ppa/backports': }
   
   exec {
     "apt-get update":
-    path => $path
+    path => $path,
+    timeout => $cmd_timeout
   }
   
   exec {
     "apt-get -qy install kubuntu-desktop":
-    path => $path
+    path => $path,
+    timeout => $cmd_timeout
   }
   
   exec {
     "apt-get -qy remove --purge ubuntu-desktop":
-    path => $path
+    path => $path,
+    timeout => $cmd_timeout
   }
   
   exec {
     "dist-upgrade":
     command => "apt-get -qy dist-upgrade",
     path => $path,
+    timeout => $cmd_timeout
   }
   
   # Install text editors
@@ -45,52 +50,90 @@ node default {
   exec {
     "apt-get -qy install ttf-mscorefonts-installer firefox curl kwrite kdiff3 kruler kcolorchooser kcharselect davfs2":
     path => $path,
+    timeout => $cmd_timeout
   }
     
   exec {
-    "/bin/echo 'buf_size        512                 # KiByte' >> $davfs_conf_path":
+    "echo 'buf_size        512                 # KiByte' >> $davfs_conf_path":
     path => $path,
+    timeout => $cmd_timeout
   }
   exec {
-    "/bin/echo 'use_locks       0' >> $davfs_conf_path":
+    "echo 'use_locks       0' >> $davfs_conf_path":
     path => $path,
+    timeout => $cmd_timeout
   }
   exec {
-    "/bin/echo 'allow_cookie    1' >> $davfs_conf_path":
+    "echo 'allow_cookie    1' >> $davfs_conf_path":
     path => $path,
+    timeout => $cmd_timeout
   }
   exec {
-    "/bin/echo 'cache_size      200             # MiB' >> $davfs_conf_path":
+    "echo 'cache_size      200             # MiB' >> $davfs_conf_path":
     path => $path,
+    timeout => $cmd_timeout
   }
   exec {
-    "/bin/echo 'table_size      4096' >> $davfs_conf_path":
+    "echo 'table_size      4096' >> $davfs_conf_path":
     path => $path,
+    timeout => $cmd_timeout
   }
   exec {
-    "/bin/echo 'dir_refresh     10' >> $davfs_conf_path":
+    "echo 'dir_refresh     10' >> $davfs_conf_path":
     path => $path,
+    timeout => $cmd_timeout
   }
   exec {
-    "/bin/echo 'file_refresh    1' >> $davfs_conf_path":
+    "echo 'file_refresh    1' >> $davfs_conf_path":
     path => $path,
+    timeout => $cmd_timeout
   }
   exec {
-    "/bin/echo 'delay_upload    0' >> $davfs_conf_path":
+    "echo 'delay_upload    0' >> $davfs_conf_path":
     path => $path,
+    timeout => $cmd_timeout
   }
   exec {
-    "/bin/echo 'gui_optimize    1' >> $davfs_conf_path":
+    "echo 'gui_optimize    1' >> $davfs_conf_path":
     path => $path,
+    timeout => $cmd_timeout
   }
   
   exec {
-    "/usr/bin/apt-get -qy install okteta optipng jpegoptim":
+    "apt-get -qy install okteta optipng jpegoptim":
     path => $path,
+    timeout => $cmd_timeout
+  }
+  
+  exec {
+    "apt-get install libxss1 libappindicator1 libindicator7":
+    path => $path,
+    timeout => $cmd_timeout
+  }
+  
+  exec {
+    "wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb":
+    path => $path,
+    timeout => $cmd_timeout
+  }
+  
+  exec {
+    "dpkg -i google-chrome*.deb":
+    path => $path,
+    timeout => $cmd_timeout
+  }
+  
+  exec {
+    "apt-get -f install":
+    path => $path,
+    timeout => $cmd_timeout
+  }
+  
+  class { 'postgresql::globals':
+    version => '9.3'
   }
     
-  class { 'postgresql::server': 
-    version => '9.2',
+  class { 'postgresql::server':
     ipv4acls => [
       'local all all trust',
       'host all all 127.0.0.* trust'],
